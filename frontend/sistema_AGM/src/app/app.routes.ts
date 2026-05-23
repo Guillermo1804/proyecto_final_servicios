8dc87de1778cd3a3320bcc1d226fb0107be2cf71
Guillermo1804 <memosanchez101@gmail.com>
2026-05-23 01:15:19 -0600

    Mejora perrona del front

--- frontend/sistema_AGM/src/app/app.routes.ts
+++ frontend/sistema_AGM/src/app/app.routes.ts
@@ -1,11 +1,49 @@
+// import { Routes } from '@angular/router';
+// import { LoginScreen } from './screens/login-screen/login-screen';
+// import { authGuard, roleGuard } from './guards/auth.guard';
+// export const routes: Routes = [
+// 	{ path: '', pathMatch: 'full', redirectTo: 'login' },
+// 	{ path: 'login', component: LoginScreen },
+// 	{ path: 'admin', canMatch: [authGuard, roleGuard], data: { roles: ['admin'] }, loadChildren: () => import('./screens/admin-screen/admin.routes').then((routes) => routes.ADMIN_ROUTES) },
+// 	{ path: 'docente', canMatch: [authGuard, roleGuard], data: { roles: ['docente'] }, loadChildren: () => import('./screens/docente-screen/docente.routes').then((routes) => routes.DOCENTE_ROUTES) },
+// 	{ path: 'alumno', canMatch: [authGuard, roleGuard], data: { roles: ['alumno'] }, loadChildren: () => import('./screens/alumno-screen/alumno.routes').then((routes) => routes.ALUMNO_ROUTES) },
+// 	{ path: '**', redirectTo: 'login' }
+// ];
 import { Routes } from '@angular/router';
 import { LoginScreen } from './screens/login-screen/login-screen';
-import { authGuard, roleGuard } from './guards/auth.guard';
+// import { authGuard, roleGuard } from './guards/auth.guard';
+
 export const routes: Routes = [
 	{ path: '', pathMatch: 'full', redirectTo: 'login' },
+
 	{ path: 'login', component: LoginScreen },
-	{ path: 'admin', canMatch: [authGuard, roleGuard], data: { roles: ['admin'] }, loadChildren: () => import('./screens/admin-screen/admin.routes').then((routes) => routes.ADMIN_ROUTES) },
-	{ path: 'docente', canMatch: [authGuard, roleGuard], data: { roles: ['docente'] }, loadChildren: () => import('./screens/docente-screen/docente.routes').then((routes) => routes.DOCENTE_ROUTES) },
-	{ path: 'alumno', canMatch: [authGuard, roleGuard], data: { roles: ['alumno'] }, loadChildren: () => import('./screens/alumno-screen/alumno.routes').then((routes) => routes.ALUMNO_ROUTES) },
+
+	{
+		path: 'admin',
+		// canMatch: [authGuard, roleGuard],
+		// data: { roles: ['admin'] },
+		loadChildren: () =>
+			import('./screens/admin-screen/admin.routes')
+				.then((routes) => routes.ADMIN_ROUTES)
+	},
+
+	{
+		path: 'docente',
+		// canMatch: [authGuard, roleGuard],
+		// data: { roles: ['docente'] },
+		loadChildren: () =>
+			import('./screens/docente-screen/docente.routes')
+				.then((routes) => routes.DOCENTE_ROUTES)
+	},
+
+	{
+		path: 'alumno',
+		// canMatch: [authGuard, roleGuard],
+		// data: { roles: ['alumno'] },
+		loadChildren: () =>
+			import('./screens/alumno-screen/alumno.routes')
+				.then((routes) => routes.ALUMNO_ROUTES)
+	},
+
 	{ path: '**', redirectTo: 'login' }
 ];
